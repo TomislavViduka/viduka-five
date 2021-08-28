ee6c6d3eb8170b3abbe843100cf7be3c2ce7dd6b
TomislavViduka <tomislav.viduka@gmail.com>
2021-08-28 18:05:14 +0200

    Update WeatherForecast.cs

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,5 +11,8 @@ namespace VidukaFiveNews
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public int Temp;
     }
+
 }
