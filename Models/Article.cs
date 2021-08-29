a4f290718f6b4ce12d536d02afd0afdb66b227cb
TomislavViduka <tomislav.viduka@gmail.com>
2021-08-29 17:39:04 +0200

    Models and DB

---
+++ Models/Article.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace VidukaFiveNews.Models
+{
+    public class Article
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("date")]
+        public int Date { get; set; }
+
+        [JsonPropertyName("authorId")]
+        [ForeignKey("authorId")]
+        public int AuthorId { get; set; }
+
+        public Article()
+        {
+        }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace VidukaFiveNews.Models
+{
+    public class Author
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+
+        [JsonPropertyName("q")]
+        public string Q { get; set; }
+
+        public Author()
+        {
+        }
+    }
+}
